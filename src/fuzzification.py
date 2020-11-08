52c8d465f543be3ce82e53a8890799df54071c62
Ginrod <gilbecubano@gmail.com>
2020-11-07 22:06:57 -0500

    refactoring Fluctuation functions and adding Purchases ones

--- src/fuzzification.py
+++ src/fuzzification.py
@@ -12,19 +12,31 @@ class Fluctuation:
     
     @staticmethod
     def very_negative(x):
-        return -10000 <= x and x <= -1000 and (1000 + x) / (1000 - 10000)  or 0
+        if x < -10000: return 1
+        elif -10000 <= x and x <= -1000: return (1000 + x) / (1000 - 10000)
+        # if x > -1000
+        return 0
 
     @staticmethod
     def few_negative(x):
-        return -1000 <= x and x <= -10 and (10 + x) / (10 - 1000)  or 0
+        if x < -1000: return 1
+        elif -1000 <= x and x <= -10: return (10 + x) / (10 - 1000)
+        # if x > -10
+        return 0
 
     @staticmethod
     def negligible(x):
-        return -10 <= x and x <= 0 and (10 + x) / 10 or 0
-        
+        if x < -10: return 0
+        elif -10 <= x and x <= 0: return (10 + x) / 10
+        # if x > 0
+        return 1
+
     @staticmethod
     def positive(x):
-        return 0 <= x and x <= 10000 and (10000 - (10000 - x)) / 10000 or 0
+        if x < 0: return 0
+        elif 0 <= x and x <= 10000: return (10000 - (10000 - x)) / 10000
+        # if x > 10000
+        return 1
     
     @staticmethod
     def plot(func):
@@ -39,3 +51,38 @@ class Fluctuation:
 # Fluctuation.plot(Fluctuation.few_negative)
 # Fluctuation.plot(Fluctuation.negligible)
 # Fluctuation.plot(Fluctuation.positive)
+
+class Purchases:
+    
+    @staticmethod
+    def few(x):
+        if x < 0: return 1
+        elif 0 <= x and x <= 1000: return (1000 - x) / 1000
+        # if x > 1000
+        return 0
+
+    @staticmethod
+    def medium(x):
+        if 1000 <= x and x <= 4500: return (4500 - 1000 - (4500 - x)) / (4500 - 1000)
+        elif 4500 < x and x <= 10000: return (10000 - x) / (10000 - 4500)
+        # if x < 1000 or x > 10000
+        return 0
+    
+    @staticmethod
+    def high(x):
+        if x < 10000: return 0
+        elif 10000 <= x and x <= 50000: return (50000 - 10000 - (50000 - x)) / (50000 - 10000)
+        # if x > 50000
+        return 1
+
+    @staticmethod
+    def plot(func):
+        rank = func.__name__ == 'few' and (0, 1000) or \
+               func.__name__ == 'medium' and (1000, 10000) or \
+               func.__name__ == 'high' and (10000, 50000)
+        
+        general_plot(func, rank)
+
+# Purchases.plot(Purchases.few)
+# Purchases.plot(Purchases.medium)
+# Purchases.plot(Purchases.high)
