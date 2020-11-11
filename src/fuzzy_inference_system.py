10aeb0f4929a9cf315e3dd647ff5952f45b689a1
Ginrod <gilbecubano@gmail.com>
2020-11-10 20:35:22 -0500

    implement COA defuzzification method

--- src/fuzzy_inference_system.py
+++ src/fuzzy_inference_system.py
@@ -22,3 +22,14 @@ class FuzzyInferenceSystem:
             rule_fuzzy_subset_funcs.append(func)
         
         return lambda x: max([func(x) for func in rule_fuzzy_subset_funcs])
+    
+    def COA(self, ruled_fuzzy_set_func, domain=None):
+        inf, sup = domain or self.output_variable.domain
+
+        num, dem = 0, 0
+        for zj in range(inf, sup + 1, 0.01):
+            miu_zj = ruled_fuzzy_set_func(zj)
+            num += miu_zj * zj
+            dem += miu_zj
+        
+        return num / dem
